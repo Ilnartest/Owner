fbece5f53aea7ffcd7b95a73b39608f69a7b36c2
Denis Litinskiy <plnvoran@mail.ru>
2023-05-28 10:06:55 +0300

    first commit

---
+++ src/test/java/owner/WebTest.java
@@ -0,0 +1,19 @@
+package owner;
+
+import owner.config.WebDriverConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WebTest extends TestBase {
+
+    @Test
+    void githubSimpleTest() {
+
+        open("/");
+        $(".eyebrow-banner .pr-3 .color-fg-subtle").shouldHave(text("Your AI pair programmer is leveling up"));
+
+    }
+}
